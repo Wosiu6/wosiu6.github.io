1466bd3a48c2a62525c711d8fd9d3c28e8f19e18
wosiu6 <wos.patryk3@gmail.com>
2024-03-08 22:35:36 +0000

    Added a few links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import './Footer.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter, faLinkedin, faSpotify, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter, faLinkedin, faLastfm, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faFile } from '@fortawesome/free-solid-svg-icons'
 
 const Footer: React.FC = () => {
 
   // @ts-ignore
-  const myIcon : IconProp = "fa-kit fa-my-icon";
+  const myIcon: IconProp = "fa-kit fa-my-icon";
 
   return (
     <footer className="text-center bg-dark text-white py-3">
       <p>© 2024 Patryk Wos</p>
       <div className="social-media">
         <a href="https://www.facebook.com/wosiu6" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faFacebook} />
+          <FontAwesomeIcon icon={faFacebook} />
         </a>
         <a href="https://twitter.com/wosiu6" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faTwitter} />
@@ -22,7 +23,18 @@ const Footer: React.FC = () => {
         <a href="https://github.com/wosiu6" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faGithub} />
         </a>
-        {/* Add more social media links as needed */}
+        <a href="https://linkedin.com/in/patryk-wos-68347b17b" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href="https://www.last.fm/user/wosiu6" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLastfm} />
+        </a>
+        <a href="Patryk Wos - Resume - Software Engineer.txt" target="_blank" title="CV" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFile} />
+        </a>
+      </div>
+      <div className="attributions">
+        <a href="https://www.flaticon.com/authors/freepik" target="_blank" rel="noopener noreferrer" title="icons">Icons by Freepik</a>
       </div>
     </footer>
   );
