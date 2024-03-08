0b8651b434d93ea4cef414d1573f6ac52ec8384e
wosiu6 <wos.patryk3@gmail.com>
2024-03-08 20:12:53 +0000

    fix for favico v2

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import './Header.css';
 
 const Header: React.FC = () => {
   const [imageIndex, setImageIndex] = useState(0);
-  const images = ['background_1.jpg', 'background_2.jpg', 'background_3.jpg', 'background_4.jpg', 'background_5.jpg', 'background_6.jpg', 'background_7.jpg'];
+  const images = ['background_1.jpg', 'background_2.jpg', 'background_3.jpg', 'background_4.jpg', 'background_5.jpg', 'background_6.jpg'];
   const [currentImage, setCurrentImage] = useState(images[0]);
 
   useEffect(() => {
