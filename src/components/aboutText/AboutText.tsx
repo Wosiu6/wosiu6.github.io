462255aca7043fd450cf433308f5886b0c3ed22a
wosiu6 <wos.patryk3@gmail.com>
2024-03-21 18:52:17 +0000

    about update

--- src/components/aboutText/AboutText.tsx
+++ src/components/aboutText/AboutText.tsx
@@ -6,7 +6,8 @@ const AboutText: React.FC = () => {
     <div className="container py-5">
       <h2 className="text-center">About</h2>
       <div className="row">
-        <p>I do backend. I do not know why I made this so pretty.</p>
+        <p>My name is Patryk. I like playing games and staying active. I am a big fan of technology and anything futuristic, Cyberpunk for instance.</p>
+        <p>I mainly do backend. I do not know why I decided to put so much effort into this site.</p>
       </div>
       <a
       href="#/CV/"
