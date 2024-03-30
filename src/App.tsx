bf3dacd54c0e9f8b052b992e65205aa35e389e1c
wosiu6 <wos.patryk3@gmail.com>
2024-03-30 16:10:36 +0000

    google analytics proper implementation

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,13 @@ import Intro from './components/intro/Intro'
 import MiscList from './components/miscList/MiscList'
 import AboutText from './components/aboutText/AboutText'
 import CVDocument from './components/cv/CV'
-import GA4React from "ga-4-react";
 import SiteDesciprtion from './components/aboutSite/AboutSite'
 import NotFound from './components/notFound/NotFound'
+import useAnalytics from './useAnalytics'
 
-const ga4react = new GA4React(
-  'G-LEDJEQBVDX',
-  { /* ga custom config, optional */ },
-  [ /* additional code, optional */],
-  5000 /* timeout, optional, defaults is 5000 */
-);
-
-ga4react.initialize();
 
 export function Home() {
+  useAnalytics();
   return (
     <div className="App">
       <Header />
