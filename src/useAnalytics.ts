cc26d661e0e4794483744d5dd44e115d384e03ad
wosiu6 <wos.patryk3@gmail.com>
2024-04-16 21:28:27 +0100

    improved ga

--- src/useAnalytics.ts
+++ src/useAnalytics.ts
@@ -4,13 +4,12 @@ import { useLocation } from 'react-router-dom'
 import * as analytics from './ga4'
 
 export function useAnalytics() {
-
   React.useEffect(() => {
     analytics.init()
   }, [])
 
   React.useEffect(() => {
-    const path = location.href
+    const path = window.location.href;
     console.log(path);
     analytics.sendPageview(path)
   }, [location])
