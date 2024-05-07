77a916e798573567e53014a7302e3c544fe1fd5b
wosiu6 <wos.patryk3@gmail.com>
2024-05-07 14:54:09 +0100

    Rewording

--- src/components/projectsComponents/HTLBProjectComponent.tsx
+++ src/components/projectsComponents/HTLBProjectComponent.tsx
@@ -8,7 +8,7 @@ const HTLBProjectComponent: React.FC = () => {
     'hltb_1.png'
   ];
   const description = 'This project represents my initial foray into developing .NET APIs. It focuses on data extraction from the hidden HLTB (How Long To Beat) endpoints.  A revised version is currently underway to optimize the approach.';
-  const alertText = 'The project is undergoing a rewrite to address identified areas for improvement. The initial decision to utilize an API is being reconsidered in favor of a different approach.';
+  const alertText = 'The project is undergoing a rewrite. The initial decision to utilize an API is being reconsidered in favor of a different approach.';
   const name = 'HLTB API';
   const sourceUrl = 'https://github.com/Wosiu6/';
 
