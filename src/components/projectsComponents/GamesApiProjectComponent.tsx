5895d549d49a93742d344c52aee234a774502c70
Pat <wos.patryk3@gmail.com>
2025-08-25 22:02:56 +0100

    adjustments

--- src/components/projectsComponents/GamesApiProjectComponent.tsx
+++ src/components/projectsComponents/GamesApiProjectComponent.tsx
@@ -5,9 +5,11 @@ import React from 'react';
 const GamesApiProjectComponent: React.FC = () => {
   const imageUrls = [
     'projects/games_api_1.png',
-    'projects/games_api_2.png'
+    'projects/games_api_2.png',
+    'projects/games_api_3.png',
+    'projects/games_api_4.png'
   ];
-  const description = 'This project focuses on showcasing .NET abilities.';
+  const description = 'This project focuses on showcasing .NET abilities. Both APIs are implemented minially using minimal API approach. The main purpose of this project was to learn MediatR library and CQRS pattern. The project is documented using Swagger, which makes it easy to test and understand the available endpoints. This project utilises JWT authentication to secure certain endpoints, ensuring that only authorized users can access specific resources.';
   const name = 'Games API';
   const sourceUrl = 'https://github.com/Wosiu6/games-api';
 
